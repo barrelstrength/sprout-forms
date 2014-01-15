52bd5b993256bb5cb9649ecc012acc2ef68077b2
Noah Welch <noah@noahwelch.com>
2014-01-15 00:51:44 -0500

    Began working on display of result entries
    Signed-off-by: Noah Welch <noah@noahwelch.com>

---
+++ resources/js/scripts.js
@@ -0,0 +1,28 @@
+
+$(document).ready(function() {
+
+
+	var viewHeight = $( window ).height();
+	
+	var viewHeight = (viewHeight/10)*6;
+	
+	$("#content").css({
+		"height": viewHeight
+	});
+	
+	
+	/*
+
+	$('#content').jScrollPane({
+		showArrows: true,
+		hideFocus: true
+	});
+	
+	$('.entries-container').jScrollPane({
+		showArrows: true,
+		hideFocus: true
+	});
+	
+*/
+	
+});
