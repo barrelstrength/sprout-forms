f161cb8f4a4fc46f0eb5a9cd0fe1d4825c0e6cee
Noah Welch <noah@noahwelch.com>
2014-03-25 17:46:21 -0400

    Fixed a number of issues caused by Craft 1.4. Updated JS to dynamically show and hide "Powered by" footer when needed.
    Signed-off-by: Noah Welch <noah@noahwelch.com>

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,7 +1,19 @@
+var element = $('#tab-entries').jScrollPane({
+	showArrows: true,
+	hideFocus: true
+});
+var api = element.data('jsp');
+var horizontalAmmt = api.getContentPositionX();
+var horizontalAmmt = -horizontalAmmt;
+var dateHeight = $("td.date").innerHeight();
+
+// Avoid future issues with pane padding changing by making content negative margin dynamic
+var panePaddingLeft = $('.pane').css('padding-left');
+var panePaddingRight = $('.pane').css('padding-right');
+var panePaddingBottom = $('.pane').css('padding-bottom');
 
 function getContentHeight() {
 	var viewHeight = $( window ).height();
-	
 	var viewHeight = (viewHeight/10)*6.5;
 	
 	if (viewHeight > 500) {
@@ -13,8 +25,6 @@ function getContentHeight() {
 		
 		setTimeout(function(){api.scrollToX(0, 0)}, 100);
 		setTimeout(function(){api.reinitialise()}, 400);
-		
-		
 	} else {
 		$("#tab-entries").css({
 			"height": "500px"
@@ -22,30 +32,24 @@ function getContentHeight() {
 	}
 }
 
-var element = $('#tab-entries').jScrollPane({
-	showArrows: true,
-	hideFocus: true
-});
-
-var api = element.data('jsp');
-
-var horizontalAmmt = api.getContentPositionX();
-
-var horizontalAmmt = -horizontalAmmt;
-
-var dateHeight = $("td.date").innerHeight();
-	
-	
+function checkSelectedTab() {
+	if ($("#tab-formEntries").hasClass("sel")) {
+		$('.bsd-branding').addClass('hidden');
+		
+		$('#content').css({
+			'margin-left' : '-' + panePaddingLeft,
+			'margin-right' : '-' + panePaddingRight,
+			'margin-bottom' : '-' + panePaddingBottom
+		});
+	}
+}
 	
 $(function() {
 	$('#tab-entries').bind({
 		'jsp-scroll-x': function(event, scrollPositionX, isAtLeft, isAtRight) {
 		
-			
-
 			$(".left-border").css({
 				"left" : scrollPositionX
-				
 			});
 			
 			$(".date").css({
@@ -54,24 +58,21 @@ $(function() {
 			
 			$("table#entries td.delete").css({
 				"right" : (-scrollPositionX)+16
-				
 			});
-			
-			
 		},
 		
 		'jsp-scroll-y': function(event, scrollPositionY, isAtTop, isAtBottom) {
 			$("th div").css({
 				"top" : scrollPositionY
 			});
-			
 		}
 	})
 });
-	
+
+
 $(document).ready(function() {
-	
 	getContentHeight();
+	checkSelectedTab();
 	
 	$('#tab-entries').jScrollPane({
 		showArrows: true,
@@ -83,20 +84,42 @@ $(document).ready(function() {
 	});
 	
 	$("th > div").each(function() {
-		var divWidth = $(this).parent("th").innerWidth();
+		if ($(this).parent("th").hasClass("padding")) {
+		
+			var divWidth = ($('.date').innerWidth()) + ($('.left-border').innerWidth());
+		} else {
+			var divWidth = $(this).parent("th").innerWidth();
+		}
+	
+		
 		var divHeight = $(this).parent("th").innerHeight();
 		
 		$(this).css({
 			"width" : divWidth,
 			"height": divHeight
 		});
-		
 	});
 	
+	$('a').click(function(event) {
+
+		var currentUrl = String(event.currentTarget);
+		var currentSegment = currentUrl.split("#")[1];
+		
+		//alert(currentSegment);
+	
+		if (currentSegment == "tab-entries") {
+			$('.bsd-branding').addClass('hidden');
+			$('#content').css({
+				'margin' : '0 -24px -24px'
+				
+			});
+			
+		} else {
+			$('.bsd-branding').removeClass('hidden');
+		}
+	});
 });
 
 $( window ).resize(function() {
 	getContentHeight();
-	
-	
 });
