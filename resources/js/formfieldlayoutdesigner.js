37a3461d457b8823d3f1be5c532a6695a744232e
Ben Parizek <ben@barrelstrengthdesign.com>
2014-10-04 14:07:34 -0400

    Improve redirects and messaging when form changes will be lost

--- resources/js/formfieldlayoutdesigner.js
+++ resources/js/formfieldlayoutdesigner.js
@@ -18,6 +18,7 @@ if (typeof Sprout == 'undefined')
 Sprout.FormFieldLayoutDesigner = Craft.FieldLayoutDesigner.extend({
 
 	formId: null,
+	isDirty: false,
 
 	// constructor: function(name) {
  //    this.name = name;
@@ -61,12 +62,27 @@ Sprout.FormFieldLayoutDesigner = Craft.FieldLayoutDesigner.extend({
 		// End copied init() parent method
 		// ------------------------------------------------------------
 
+		// Track any changes in the form so we can prompt the user with better warning 
+		// messages if they try to navigate away before saving
+		$(':input').change(function () {
+			isDirty = true;
+		});
+
 		this.addListener($('#newfieldbtn a'), 'activate', 'confirmSavedChanges');
 	},
 	
 	confirmSavedChanges: function(event) 
 	{
-		var response = confirm(Craft.t('Be sure to save any changes you made to your form before creating or editing a field. All changes to your form and field layout that have not been saved will be lost when you navigate away from this page.'));
+		if (this.isDirty)
+		{
+			// If our form has not changed, warn user they will be losing changes
+			var response = confirm(Craft.t('Be sure to save any changes you made to your form before creating or editing a field. All changes to your form and field layout that have not been saved will be lost when you navigate away from this page.'));
+		}
+		else
+		{
+			// If our form has not changed, go to the next page
+			var response = true;
+		}
 
 		if (response == true) 
 		{ 
