13e4dcea7b38d7205c4b6ebb4ad0e34f2546ebe4
Selvin Ortiz <selvin@selvin.co>
2015-01-09 13:25:52 -0600

    Globals Issue #265821 - Updates the places where context is switch and reset - Updates the environment by triggering it before context switching - Fixes issue where globals would disappear after a failed submission

--- controllers/SproutForms_EntriesController.php
+++ controllers/SproutForms_EntriesController.php
@@ -5,33 +5,38 @@ class SproutForms_EntriesController extends BaseController
 {
 	/**
 	 * Allow anonymous execution
-	 * 
+	 *
 	 * @var bool
 	 */
 	protected $allowAnonymous = array('actionSaveEntry');
-	
+
 	public $form;
 
 	/**
 	 * Process form submission
-	 * 
+	 *
+	 * @throws Exception
+	 * @throws HttpException
 	 * @return void
 	 */
 	public function actionSaveEntry()
-	{	
+	{
 		$this->requirePostRequest();
+
+		// Triggers loading of environment (globals) before context switching
+		craft()->templates->renderObjectTemplate('{env}', array('env' => true));
 
 		$formHandle = craft()->request->getRequiredPost('handle');
 		$this->form = craft()->sproutForms_forms->getFormByHandle($formHandle);
 
-		if (!isset($this->form)) 
+		if (!isset($this->form))
 		{
 			throw new Exception(Craft::t('No form exists with the handle “{handle}”', array('handle' => $formHandle)));
 		}
 
-		$oldFieldContext = craft()->content->fieldContext;
-		$oldContentTable = craft()->content->contentTable;
-
+		// Switching context to let craft know about our custom entry content
+		$oldFieldContext               = craft()->content->fieldContext;
+		$oldContentTable               = craft()->content->contentTable;
 		craft()->content->fieldContext = $this->form->getFieldContext();
 		craft()->content->contentTable = $this->form->getContentTable();
 
@@ -45,14 +50,14 @@ class SproutForms_EntriesController extends BaseController
 		$this->_populateEntryModel($entry);
 
 		// Swap out any dynamic variables for our notifications
-		$this->form->notificationRecipients = craft()->templates->renderObjectTemplate($this->form->notificationRecipients, $entry);
-		$this->form->notificationSubject = craft()->templates->renderObjectTemplate($this->form->notificationSubject, $entry);
-		$this->form->notificationSenderName = craft()->templates->renderObjectTemplate($this->form->notificationSenderName, $entry);
-		$this->form->notificationSenderEmail = craft()->templates->renderObjectTemplate($this->form->notificationSenderEmail, $entry);
+		$this->form->notificationRecipients   = craft()->templates->renderObjectTemplate($this->form->notificationRecipients, $entry);
+		$this->form->notificationSubject      = craft()->templates->renderObjectTemplate($this->form->notificationSubject, $entry);
+		$this->form->notificationSenderName   = craft()->templates->renderObjectTemplate($this->form->notificationSenderName, $entry);
+		$this->form->notificationSenderEmail  = craft()->templates->renderObjectTemplate($this->form->notificationSenderEmail, $entry);
 		$this->form->notificationReplyToEmail = craft()->templates->renderObjectTemplate($this->form->notificationReplyToEmail, $entry);
 
-		if (craft()->sproutForms_entries->saveEntry($entry)) 
-		{	
+		if (craft()->sproutForms_entries->saveEntry($entry))
+		{
 			if (craft()->request->isAjaxRequest())
 			{
 				$return['success'] = true;
@@ -62,33 +67,36 @@ class SproutForms_EntriesController extends BaseController
 			else
 			{
 				// Only send notification email for front-end submissions
-				if (!craft()->request->isCpRequest()) 
+				if (!craft()->request->isCpRequest())
 				{
 					$this->_notifyAdmin($this->form, $entry);
 				}
-				
+
 				craft()->userSession->setNotice(Craft::t('Entry saved.'));
-				
+
 				// Store our new entry so we can recreate the Entry object on our thank you page
 				$_SESSION['lastEntryId'] = $entry->id;
-				
+
 				$this->redirectToPostedUrl();
 			}
 		}
 		else
-		{	
+		{
 			if (craft()->request->isAjaxRequest())
 			{
-				$this->returnJson(array(
-					'errors' => $entry->getErrors(),
-				));
+				$this->returnJson(
+					array(
+						'errors' => $entry->getErrors(),
+					)
+				);
 			}
 			else
 			{
+				// Switching context back so any final rendering has proper context
 				craft()->content->fieldContext = $oldFieldContext;
 				craft()->content->contentTable = $oldContentTable;
 
-				if (craft()->request->isCpRequest()) 
+				if (craft()->request->isCpRequest())
 				{
 					// make errors available to variable
 					craft()->userSession->setError(Craft::t('Couldn’t save entry.'));
@@ -98,13 +106,15 @@ class SproutForms_EntriesController extends BaseController
 					craft()->sproutForms_forms->activeCpEntry = $entry;
 
 					// Return the form as an 'entry' variable if in the cp
-					craft()->urlManager->setRouteVariables(array(
-						'entry' => $entry
-					));
+					craft()->urlManager->setRouteVariables(
+						array(
+							'entry' => $entry
+						)
+					);
 				}
 				else
 				{
-					if (craft()->sproutForms_entries->fakeIt) 
+					if (craft()->sproutForms_entries->fakeIt)
 					{
 						$this->redirectToPostedUrl();
 					}
@@ -113,31 +123,33 @@ class SproutForms_EntriesController extends BaseController
 						// Store this Entry Model in a variable in our Service layer
 						// so that we can access the error object from our displayForm() variable 
 						craft()->sproutForms_forms->activeEntries[$this->form->handle] = $entry;
-						
+
 						// Return the form using it's name as a variable on the front-end
-						craft()->urlManager->setRouteVariables(array(
-							$this->form->handle => $entry
-						));
+						craft()->urlManager->setRouteVariables(
+							array(
+								$this->form->handle => $entry
+							)
+						);
 					}
 				}
-			}	
-			
+			}
+
 		}
 	}
 
 	/**
 	 * Delete an entry.
-	 * 
+	 *
 	 * @return void
 	 */
 	public function actionDeleteEntry()
-	{	
+	{
 		$this->requirePostRequest();
-		
+
 		// Get the Entry
 		$entryId = craft()->request->getRequiredPost('entryId');
-		$entry = craft()->sproutForms_entries->getEntryById($entryId);
-		
+		$entry   = craft()->sproutForms_entries->getEntryById($entryId);
+
 		// @TODO - handle errors
 		$success = craft()->sproutForms_entries->deleteEntry($entry);
 
@@ -176,11 +188,12 @@ class SproutForms_EntriesController extends BaseController
 	 * Populate a SproutForms_EntryModel with post data
 	 *
 	 * @access private
+	 *
 	 * @param SproutForms_EntryModel $entry
 	 */
 	private function _populateEntryModel(SproutForms_EntryModel $entry)
 	{
-		$entry->formId = $this->form->id;
+		$entry->formId    = $this->form->id;
 		$entry->ipAddress = craft()->request->getUserHostAddress();
 		$entry->userAgent = craft()->request->getUserAgent();
 
@@ -189,22 +202,23 @@ class SproutForms_EntriesController extends BaseController
 		$entry->setContentFromPost($fieldsLocation);
 		$entry->setContentPostLocation($fieldsLocation);
 	}
-	
+
 	/**
 	 * Notify admin
-	 * 
+	 *
 	 * @param object $form
 	 * @param object $field
+	 *
 	 * @return bool
 	 */
 	private function _notifyAdmin(SproutForms_FormModel $form, SproutForms_EntryModel $entry)
-	{	
+	{
 		// Get our recipients
 		$recipients = explode(',', $form->notificationRecipients);
 		$recipients = array_map('trim', $recipients);
 		$recipients = array_unique($recipients);
-		
-		if ($recipients) 
+
+		if ($recipients)
 		{
 			$email = new EmailModel();
 
@@ -212,33 +226,35 @@ class SproutForms_EntriesController extends BaseController
 
 			$fields = $entry->getFieldLayout()->getFields();
 
-			$settings = craft()->plugins->getPlugin('sproutforms')->getSettings();
+			$settings               = craft()->plugins->getPlugin('sproutforms')->getSettings();
 			$templateFolderOverride = $settings->templateFolderOverride;
 
-			$emailTemplate = craft()->path->getPluginsPath() . 'sproutforms/templates/_special/';
+			$emailTemplate = craft()->path->getPluginsPath().'sproutforms/templates/_special/';
 
-			if ($templateFolderOverride) 
+			if ($templateFolderOverride)
 			{
-				$emailTemplateFile = craft()->path->getSiteTemplatesPath() . $templateFolderOverride . "/email";
+				$emailTemplateFile = craft()->path->getSiteTemplatesPath().$templateFolderOverride."/email";
 
-				foreach (craft()->config->get('defaultTemplateExtensions') as $extension) 
+				foreach (craft()->config->get('defaultTemplateExtensions') as $extension)
 				{
-					if (IOHelper::fileExists($emailTemplateFile . "." . $extension)) 
+					if (IOHelper::fileExists($emailTemplateFile.".".$extension))
 					{
-						$emailTemplate = craft()->path->getSiteTemplatesPath() . $templateFolderOverride . "/";
+						$emailTemplate = craft()->path->getSiteTemplatesPath().$templateFolderOverride."/";
 					}
 				}
 			}
 
 			// Set our Sprout Forms Email Template path
 			craft()->path->setTemplatesPath($emailTemplate);
-			
-			$email->htmlBody = craft()->templates->render('email', array(
-				'formName' => $form->name,
-				// 'entryCpUrl' => $entryCpUrl,
-				'fields' => $fields,
-				'element' => $entry
-			));
+
+			$email->htmlBody = craft()->templates->render(
+				'email', array(
+					'formName' => $form->name,
+					// 'entryCpUrl' => $entryCpUrl,
+					'fields'   => $fields,
+					'element'  => $entry
+				)
+			);
 
 			craft()->path->setTemplatesPath(craft()->path->getCpTemplatesPath());
 
@@ -253,59 +269,71 @@ class SproutForms_EntriesController extends BaseController
 			$email->subject   = $form->notificationSubject;
 
 			// Has a custom subject been set for this form?
-			if ($form->notificationSubject) 
+			if ($form->notificationSubject)
 			{
-				try {
-					$email->subject = craft()->templates->renderString($form->notificationSubject, array(
-						'entry' => $post
-					));
+				try
+				{
+					$email->subject = craft()->templates->renderString(
+						$form->notificationSubject, array(
+							'entry' => $post
+						)
+					);
 				}
-				catch (\Exception $e) {
+				catch (\Exception $e)
+				{
 					// do nothing;  retain default subj
 				}
 			}
-			
-			// custom replyTo has been set for this form
-			if ($form->notificationReplyToEmail) 
-			{
-				try {
 
-					$email->replyTo = craft()->templates->renderString($form->notificationReplyToEmail, array(
-						'entry' => $post
-					));
-					
+			// custom replyTo has been set for this form
+			if ($form->notificationReplyToEmail)
+			{
+				try
+				{
+
+					$email->replyTo = craft()->templates->renderString(
+						$form->notificationReplyToEmail, array(
+							'entry' => $post
+						)
+					);
+
 					// we must validate this before attempting to send; 
 					// invalid email will throw an error/fail to send silently
-					if ( ! $this->_validEmail($email->replyTo) ) 
+					if (!$this->_validEmail($email->replyTo))
 					{
 						$email->replyTo = null;
 					}
 				}
-				catch (\Exception $e) {
+				catch (\Exception $e)
+				{
 					// do nothing;  replyTo will not be included
 				}
 			}
-			
+
 			$error = false;
-			foreach ($recipients as $emailAddress) 
-			{	
+			foreach ($recipients as $emailAddress)
+			{
 				// Do we need to swap in any email addresses that 
 				// were submitted with the form?
-				$email->toEmail = craft()->templates->renderString($emailAddress, array(
-					'entry' => $post
-				));
-				
+				$email->toEmail = craft()->templates->renderString(
+					$emailAddress, array(
+						'entry' => $post
+					)
+				);
+
 				// we must validate this before attempting to send;
 				// invalid email will throw an error/fail to send silently
-				if ( ! $this->_validEmail($email->toEmail) ) 
+				if (!$this->_validEmail($email->toEmail))
 				{
 					continue;
 				}
-				
-				try {
+
+				try
+				{
 					$res = craft()->email->sendEmail($email);
 				}
-				catch (\Exception $e) {
+				catch (\Exception $e)
+				{
 					$error = true;
 				}
 			}
@@ -313,14 +341,15 @@ class SproutForms_EntriesController extends BaseController
 			return $error;
 		}
 	}
-	
+
 	/**
 	 * Validate email address
-	 * 
+	 *
 	 * @param  string $email recipient list email
+	 *
 	 * @return bool          true/false
 	 */
-	private function _validEmail($email) 
+	private function _validEmail($email)
 	{
 		return preg_match("/^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}$/ix", $email);
 	}
@@ -329,6 +358,7 @@ class SproutForms_EntriesController extends BaseController
 	 * Route Controller for Edit Entry Template
 	 *
 	 * @param array $variables
+	 *
 	 * @throws HttpException
 	 * @throws Exception
 	 */
@@ -344,18 +374,18 @@ class SproutForms_EntriesController extends BaseController
 		{
 			$entry = craft()->sproutForms_entries->getEntryById($entryId);
 		}
-		
+
 		$form = craft()->sproutForms_forms->getFormById($entry->formId);
 
 		// Set our Entry's Field Context and Content Table
 		craft()->content->fieldContext = $form->getFieldContext();
 		craft()->content->contentTable = $form->getContentTable();
-		
+
 		$variables['form']    = $form;
 		$variables['entryId'] = $entryId;
 
 		// This is our element, so we know where to get the field values
-		$variables['entry']   = $entry;
+		$variables['entry'] = $entry;
 
 		// Get the fields for this entry
 		$variables['tabs'] = $entry->getFieldLayout()->getTabs();
--- services/SproutForms_EntriesService.php
+++ services/SproutForms_EntriesService.php
@@ -9,13 +9,14 @@ class SproutForms_EntriesService extends BaseApplicationComponent
 
 	/**
 	 * Constructor
-	 * 
+	 *
 	 * @param object $entryRecord
 	 */
 	public function __construct($entryRecord = null)
 	{
 		$this->entryRecord = $entryRecord;
-		if (is_null($this->entryRecord)) {
+		if (is_null($this->entryRecord))
+		{
 			$this->entryRecord = SproutForms_EntryRecord::model();
 		}
 	}
@@ -28,7 +29,7 @@ class SproutForms_EntriesService extends BaseApplicationComponent
 	 * @return ElementCriteriaModel
 	 * @throws Exception
 	 */
-	public function getCriteria(array $attributes=array())
+	public function getCriteria(array $attributes = array())
 	{
 		return craft()->elements->getCriteria('SproutForms_Entry', $attributes);
 	}
@@ -37,11 +38,12 @@ class SproutForms_EntriesService extends BaseApplicationComponent
 	 * Saves a entry.
 	 *
 	 * @param SproutForms_EntryModel $entry
+	 *
 	 * @throws \Exception
 	 * @return bool
 	 */
 	public function saveEntry(SproutForms_EntryModel $entry)
-	{	
+	{
 		$isNewEntry = !$entry->id;
 
 		if ($entry->id)
@@ -60,42 +62,47 @@ class SproutForms_EntriesService extends BaseApplicationComponent
 			$entryRecord = new SproutForms_EntryRecord();
 		}
 
-		$entryRecord->formId = $entry->formId;
+		$entryRecord->formId    = $entry->formId;
 		$entryRecord->ipAddress = $entry->ipAddress;
 		$entryRecord->userAgent = $entry->userAgent;
 
 		$entryRecord->validate();
 		$entry->addErrors($entryRecord->getErrors());
 
-		
+
 		// ------------------------------------------------------------
 		// Fire 'onBeforeSaveEntry' Event
 		// ------------------------------------------------------------
-		
+
 		Craft::import('plugins.sproutforms.events.SproutForms_OnBeforeSaveEntryEvent');
 
-		$event = new SproutForms_OnBeforeSaveEntryEvent($this, array(
-			'entry'      => $entry,
-			'isNewEntry' => $isNewEntry
-		));
+		$event = new SproutForms_OnBeforeSaveEntryEvent(
+			$this, array(
+				'entry'      => $entry,
+				'isNewEntry' => $isNewEntry
+			)
+		);
 
 		craft()->sproutForms->onBeforeSaveEntry($event);
-		
+
 		// ------------------------------------------------------------
 
 		if (!$entry->hasErrors())
 		{
 			$form = craft()->sproutForms_forms->getFormById($entry->formId);
 
+			// @warning
+			// Rendering an object template is context sensitive
+			// The environment and globals should have been initialized by now
 			$entry->getContent()->title = craft()->templates->renderObjectTemplate($form->titleFormat, $entry);
 
 			$transaction = craft()->db->getCurrentTransaction() === null ? craft()->db->beginTransaction() : null;
 			try
-			{	
+			{
 				if ($event->isValid)
-				{	
+				{
 					if (craft()->elements->saveElement($entry))
-					{	
+					{
 						// Now that we have an element ID, save it on the other stuff
 						if ($isNewEntry)
 						{
@@ -113,18 +120,19 @@ class SproutForms_EntriesService extends BaseApplicationComponent
 						// ------------------------------------------------------------
 						// Fire an 'onSaveEntry' event
 						// ------------------------------------------------------------
-						
-						Craft::import('plugins.sproutforms.events.SproutForms_OnSaveEntryEvent');
-						
-						$event = new SproutForms_OnSaveEntryEvent($this, array(
-							'entry'      => $entry,
-							'isNewEntry' => $isNewEntry,
 
-							// @TODO - DEPRECATE and IMPROVE
-							// Support for Sprout Email Event
-							'event'      => 'saveEntry',
-							'entity'     => $entry,
-						));
+						Craft::import('plugins.sproutforms.events.SproutForms_OnSaveEntryEvent');
+
+						$event = new SproutForms_OnSaveEntryEvent(
+							$this, array(
+								'entry'      => $entry,
+								'isNewEntry' => $isNewEntry,
+								// @TODO - DEPRECATE and IMPROVE
+								// Support for Sprout Email Event
+								'event'      => 'saveEntry',
+								'entity'     => $entry,
+							)
+						);
 
 						craft()->sproutForms->onSaveEntry($event);
 
@@ -134,8 +142,8 @@ class SproutForms_EntriesService extends BaseApplicationComponent
 					}
 				}
 				else
-				{	
-					if ($event->fakeIt) 
+				{
+					if ($event->fakeIt)
 					{
 						// Pretend to submit the form even though it didn't submit
 						craft()->sproutForms_entries->fakeIt = true;
@@ -158,8 +166,9 @@ class SproutForms_EntriesService extends BaseApplicationComponent
 
 	/**
 	 * Deletes an entry
-	 * 
+	 *
 	 * @param int $id
+	 *
 	 * @return boolean
 	 */
 	public function deleteEntry(SproutForms_EntryModel $entry)
@@ -195,7 +204,7 @@ class SproutForms_EntriesService extends BaseApplicationComponent
 	 */
 	public function getAllEntries()
 	{
-		$attributes	= array('order' => 'name');
+		$attributes = array('order' => 'name');
 
 		return $this->getCriteria($attributes)->find();
 	}
@@ -204,6 +213,7 @@ class SproutForms_EntriesService extends BaseApplicationComponent
 	 * Returns a form entry model if one is found in the database by id
 	 *
 	 * @param int $entryId
+	 *
 	 * @return null|SproutForms_EntryModel
 	 */
 	public function getEntryById($entryId)
@@ -213,7 +223,7 @@ class SproutForms_EntriesService extends BaseApplicationComponent
 
 	/**
 	 * Returns an active or new entry model
-	 * 
+	 *
 	 * @param SproutForms_FormModel $form
 	 *
 	 * @return SproutForms_EntryModel
@@ -223,7 +233,7 @@ class SproutForms_EntriesService extends BaseApplicationComponent
 		// If a form has been submitted, use our existing EntryModel
 		// otherwise, create a new EntryModel
 		if (isset(craft()->sproutForms_forms->activeEntries[$form->handle]))
-		{	
+		{
 			return craft()->sproutForms_forms->activeEntries[$form->handle];
 		}
 
