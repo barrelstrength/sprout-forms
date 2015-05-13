49ac22d3904db485b99f737dc4cc293edd00d0f4
Selvin Ortiz <selvin@selvin.co>
2015-05-13 12:37:19 -0500

    Sprout Forms 1.0.0 - Adds docblock comments to the plugin class - Updates plugin version to 1.0.0 for commercial release - Removes test suite borrowed from the old cocktail recipes plugin
    Initial Commercial Release May, 13, 2015

--- SproutFormsPlugin.php
+++ SproutFormsPlugin.php
@@ -1,8 +1,16 @@
 <?php
 namespace Craft;
 
+/**
+ * Class SproutFormsPlugin
+ *
+ * @package Craft
+ */
 class SproutFormsPlugin extends BasePlugin
 {
+	/**
+	 * @return string
+	 */
 	public function getName()
 	{
 		$pluginName         = Craft::t('Sprout Forms');
@@ -11,21 +19,33 @@ class SproutFormsPlugin extends BasePlugin
 		return ($pluginNameOverride) ? $pluginNameOverride : $pluginName;
 	}
 
+	/**
+	 * @return string
+	 */
 	public function getVersion()
 	{
-		return '0.9.1';
+		return '1.0.0';
 	}
 
+	/**
+	 * @return string
+	 */
 	public function getDeveloper()
 	{
 		return 'Barrel Strength Design';
 	}
 
+	/**
+	 * @return string
+	 */
 	public function getDeveloperUrl()
 	{
 		return 'http://barrelstrengthdesign.com';
 	}
 
+	/**
+	 * @return bool
+	 */
 	public function hasCpSection()
 	{
 		return true;
@@ -50,78 +70,38 @@ class SproutFormsPlugin extends BasePlugin
 		);
 	}
 
+	/**
+	 * @return array
+	 */
 	public function registerCpRoutes()
 	{
 		return array(
-
-			/*
-			 * Create New Form
-			 * @controller SproutForms_FormsController
-			 * @method     actionEditFormTemplate
-			 * @template   sproutforms/templates/forms/_editForm.html
-			 */
-			'sproutforms/forms/new'                                          => array(
+			'sproutforms/forms/new' => array(
 				'action' => 'sproutForms/forms/editFormTemplate'
 			),
-			/*
-			 * Create New Form
-			 * @controller SproutForms_FormsController
-			 * @method     actionEditFormTemplate
-			 * @template   sproutforms/templates/forms/_editForm.html
-			 */
-			'sproutforms/forms/edit/(?P<formId>\d+)'                         => array(
+			'sproutforms/forms/edit/(?P<formId>\d+)' => array(
 				'action' => 'sproutForms/forms/editFormTemplate'
 			),
-			/*
-			 * Create New Field
-			 * @controller SproutForms_FieldsController
-			 * @method     actionEditFieldTemplate
-			 * @template   sproutforms/templates/forms/_editField.html
-			 */
-			'sproutforms/forms/(?P<formId>\d+)/fields/new'                   => array(
+			'sproutforms/forms/(?P<formId>\d+)/fields/new' => array(
 				'action' => 'sproutForms/fields/editFieldTemplate'
 			),
-			/*
-			 * Edit Field
-			 * @controller SproutForms_FieldsController
-			 * @method     actionEditFieldTemplate
-			 * @template   sproutforms/templates/forms/_editField.html
-			 */
 			'sproutforms/forms/(?P<formId>\d+)/fields/edit/(?P<fieldId>\d+)' => array(
 				'action' => 'sproutForms/fields/editFieldTemplate'
 			),
-			/*
-			 * Edit Entry
-			 * @controller SproutForms_EntriesController
-			 * @method     actionEditEntryTemplate
-			 * @template   sproutforms/templates/entries/_edit.html
-			 */
-			'sproutforms/entries/edit/(?P<entryId>\d+)'                      => array(
+			'sproutforms/entries/edit/(?P<entryId>\d+)' => array(
 				'action' => 'sproutForms/entries/editEntryTemplate'
 			),
-			/*
-			 * Edit Settings
-			 * @controller SproutSeo_SettingsController
-			 * @method     actionSettingsIndexTemplate
-			 * @template   sproutforms/templates/settings/index.html
-			 */
-			'sproutforms/settings'                                           => array(
+			'sproutforms/settings' => array(
 				'action' => 'sproutForms/settings/settingsIndexTemplate'
 			),
-			/*
-			 * Filter Forms by Group
-			 */
-			'sproutforms/forms/(?P<groupId>\d+)'                             =>
-				'sproutforms/forms',
-			/*
-			 * Example Form installation page
-			 */
-			'sproutforms/examples'                                           =>
-				'sproutforms/_cp/examples',
-
+			'sproutforms/forms/(?P<groupId>\d+)' => 'sproutforms/forms',
+			'sproutforms/examples' => 'sproutforms/_cp/examples',
 		);
 	}
 
+	/**
+	 * @return array
+	 */
 	public function registerUserPermissions()
 	{
 		return array(
