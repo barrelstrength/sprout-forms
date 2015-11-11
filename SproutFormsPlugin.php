11965e06676ea797dc53f716ed59b43376077aa9
Ben Parizek <ben@barrelstrengthdesign.com>
2015-11-11 11:46:10 -0800

    Adds Settings section

--- SproutFormsPlugin.php
+++ SproutFormsPlugin.php
@@ -249,6 +249,14 @@ class SproutFormsPlugin extends BasePlugin
 			new SproutFormsAssetsField(),
 		);
 	}
+
+	/**
+	 * Get Settings URL
+	 */
+	public function getSettingsUrl()
+	{
+		return 'sproutforms/settings';
+	}
 }
 
 /**
