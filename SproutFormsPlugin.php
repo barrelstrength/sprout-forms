485df6e6ba58fd5cadfb3fe1561ce83788435ab5
Ben Parizek <ben@barrelstrengthdesign.com>
2015-11-30 12:31:42 -0800

    Fixes duplicate declaration of getSettingsUrl method

--- SproutFormsPlugin.php
+++ SproutFormsPlugin.php
@@ -265,14 +265,6 @@ class SproutFormsPlugin extends BasePlugin
 			new SproutFormsAssetsField(),
 		);
 	}
-
-	/**
-	 * Get Settings URL
-	 */
-	public function getSettingsUrl()
-	{
-		return 'sproutforms/settings';
-	}
 }
 
 /**
