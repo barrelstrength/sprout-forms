855034d86540aa6283b5f042c608c9b668138903
Ben Parizek <ben@barrelstrengthdesign.com>
2015-12-02 17:30:53 -0800

    Sprout Forms v2.1.1

--- SproutFormsPlugin.php
+++ SproutFormsPlugin.php
@@ -32,7 +32,7 @@ class SproutFormsPlugin extends BasePlugin
 	 */
 	public function getVersion()
 	{
-		return '2.1.0';
+		return '2.1.1';
 	}
 
 	/**
@@ -40,7 +40,7 @@ class SproutFormsPlugin extends BasePlugin
 	 */
 	public function getSchemaVersion()
 	{
-		return '2.0.0';
+		return '2.1.1';
 	}
 
 	/**
