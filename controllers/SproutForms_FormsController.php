d58da5076fdef1779028242c465fe890faaf2ffd
Andre Lopez <andre@barrelstrengthdesign.com>
2016-02-02 17:48:35 -0500

    Fixes issue where replyTo value is empty if so replyTo = senderEmail. #703686

--- controllers/SproutForms_FormsController.php
+++ controllers/SproutForms_FormsController.php
@@ -38,6 +38,7 @@ class SproutForms_FormsController extends BaseController
 		$form->notificationSenderName   = craft()->request->getPost('notificationSenderName');
 		$form->notificationSenderEmail  = craft()->request->getPost('notificationSenderEmail');
 		$form->notificationReplyToEmail = craft()->request->getPost('notificationReplyToEmail');
+		$form->notificationReplyToEmail = $form->notificationReplyToEmail == '' ? $form->notificationSenderEmail : $form->notificationReplyToEmail ;
 		$form->notificationCc           = craft()->request->getPost('notificationCc');
 		$form->notificationBcc          = craft()->request->getPost('notificationBcc');
 		$form->enableTemplateOverrides  = craft()->request->getPost('enableTemplateOverrides');
