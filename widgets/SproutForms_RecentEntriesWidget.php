c21a49ee2e45f76d52ff7b3b83851b4bf1a131a8
Andre Lopez <andre@barrelstrengthdesign.com>
2016-02-03 16:47:39 -0500

    Adds dashboard widget with recent entries. #231629

---
+++ widgets/SproutForms_RecentEntriesWidget.php
@@ -0,0 +1,71 @@
+<?php
+namespace Craft;
+
+class SproutForms_RecentEntriesWidget extends BaseWidget
+{
+	protected $colspan = 2;
+
+	public function getName()
+	{
+		$name = Craft::t('Sprout Forms');
+
+		// Concat form name if the user select a specific form
+		if($this->getSettings()->form != 0 && $this->getSettings()->form != null)
+		{
+			$form = sproutForms()->forms->getFormById($this->getSettings()->form);
+
+			if($form)
+			{
+				$name .= ': ' . $form->name;
+			}
+		}
+
+		return $name;
+	}
+
+	public function getBodyHtml()
+	{
+		$settings = $this->getSettings();
+		// Get the SproutForms_Entry Element type criteria
+		$criteria = craft()->elements->getCriteria('SproutForms_Entry');
+
+		if ($settings->form != 0)
+		{
+			$criteria->formId = $settings->form;
+		}
+		$criteria->limit = $settings->limit;
+
+		return craft()->templates->render('sproutforms/_widgets/recententries/body', array(
+			'entries'  => $criteria->find(),
+			'settings' => $settings
+		));
+	}
+
+	public function getSettingsHtml()
+	{
+		$forms = array(0 => Craft::t('All forms'));
+
+		$sproutForms = sproutForms()->forms->getAllForms();
+		if ($sproutForms)
+		{
+			foreach ($sproutForms as $form)
+			{
+				$forms[$form->id] = $form->name;
+			}
+		}
+
+		return craft()->templates->render('sproutforms/_widgets/recententries/settings', array(
+				'settings'    => $this->getSettings(),
+				'sproutForms' => $forms
+		));
+	}
+
+	protected function defineSettings()
+	{
+		return array(
+		   'form'     => array(AttributeType::Number, 'required' => true),
+		   'limit'    => array(AttributeType::Number, 'min' => 0, 'default' => 10),
+		   'showDate' => array(AttributeType::String)
+		);
+	}
+}
