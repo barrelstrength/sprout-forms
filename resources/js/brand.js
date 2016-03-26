fc69f1157914041d1f332881c11c73226c81f917
Ben Parizek <ben@barrelstrengthdesign.com>
2016-03-26 08:05:15 -0700

    Updates display of footer branding

--- resources/js/brand.js
+++ resources/js/brand.js
@@ -2,14 +2,14 @@
 
 	Craft.SproutBrand = Garnish.Base.extend(
 	{
-		displayFooter: function(plugin) {
+		displayFooter: function (plugin) {
 			brandHtml = '<ul>';
 			brandHtml += '<li><a href="' + plugin.pluginUrl + '" target="_blank">' + plugin.pluginName + '</a> ' + plugin.pluginVersion + '</li>';
 			brandHtml += '<li>' + plugin.pluginDescription + '</li>';
 			brandHtml += '<li> Designed by <a href="' + plugin.developerUrl + '" target="_blank">' + plugin.developerName + '</a></li>';
 			brandHtml += '</ul>';
 
-			$('#footer').prepend(brandHtml)
+			$('#footer').append(brandHtml)
 		}
 	});
 
