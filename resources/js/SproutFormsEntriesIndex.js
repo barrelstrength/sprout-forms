156aafdd521f9202075694299832aa125a45b05d
Ben Parizek <ben@barrelstrengthdesign.com>
2016-03-26 10:29:00 -0700

    Adds initial commit of Entries Explorer Chart

---
+++ resources/js/SproutFormsEntriesIndex.js
@@ -0,0 +1,23 @@
+if (typeof Craft.SproutForms === typeof undefined) {
+	Craft.SproutForms = {};
+}
+
+/**
+ * Class Craft.SproutForms.EntriesIndex
+ */
+Craft.SproutForms.EntriesIndex = Craft.BaseElementIndex.extend({
+	getViewClass: function(mode) {
+		switch (mode) {
+			case 'table':
+				return Craft.SproutForms.EntriesTableView;
+			default:
+				return this.base(mode);
+		}
+	},
+	getDefaultSort: function() {
+		return ['dateCreated', 'desc'];
+	}
+});
+
+// Register the SproutForms EntriesIndex class
+Craft.registerElementIndexClass('SproutForms_Entry', Craft.SproutForms.EntriesIndex);
--- resources/js/sproutentryindex.js
+++
@@ -1,8 +0,0 @@
-var EntryIndex = Craft.BaseElementIndex.extend();
-
-EntryIndex.prototype.getDefaultSort = function()
-{
-	return ['dateCreated', 'desc'];
-};
-
-Craft.registerElementIndexClass('SproutForms_Entry', EntryIndex);
