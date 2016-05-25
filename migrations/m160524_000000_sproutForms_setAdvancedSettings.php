71993cfa34155f4847cf29b2f95518663a10e428
Andre Lopez <andre@barrelstrengthdesign.com>
2016-05-25 09:15:02 -0400

    Adds migration to set advance settings. #880172

---
+++ migrations/m160524_000000_sproutForms_setAdvancedSettings.php
@@ -0,0 +1,58 @@
+<?php
+namespace Craft;
+
+/**
+ * The class name is the UTC timestamp in the format of mYYMMDD_HHMMSS_pluginHandle_migrationName
+ */
+class m160524_000000_sproutForms_setAdvancedSettings extends BaseMigration
+{
+	/**
+	 * Any migration code in here is wrapped inside of a transaction.
+	 *
+	 * @return bool
+	 */
+	public function safeUp()
+	{
+		$tableName = 'sproutforms_forms';
+
+		if (craft()->db->tableExists($tableName))
+		{
+			$payloadForward = craft()->db->createCommand()
+				->select('*')
+				->from($tableName)
+				->where('submitAction != null')
+				->queryRow();
+
+			$templateOverride = craft()->db->createCommand()
+				->select('*')
+				->from($tableName)
+				->where('enableTempalteOverrides = 1')
+				->queryRow();
+
+			$settings = craft()->plugins->getPlugin('sproutforms')->getSettings();
+			$settings['enablePerFormTemplateFolderOverride'] = "";
+			$settings['enablePayloadForwarding'] = "";
+
+			if ($payloadForward)
+			{
+				$settings['enablePayloadForwarding'] = "1";
+			}
+
+			if ($templateOverride)
+			{
+				$settings['enablePerFormTemplateFolderOverride'] = "1";
+			}
+
+			$settings = JsonHelper::encode($seoSettings);
+
+			craft()->db->createCommand()->update('plugins',
+				array('settings' => $settings),
+				array('class' => 'SproutForms')
+			);
+
+			Craft::log('Set advanced settings after update status id to entries', LogLevel::Info, true);
+		}
+
+		return true;
+	}
+}
