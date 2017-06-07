2a267bffc44835aa702135bc6b491dd4a14d3e7c
Andre Lopez <andre@barrelstrengthdesign.com>
2017-06-07 12:39:17 -0400

    Fixes small bug

--- src/resources/js/groups.js
+++ src/resources/js/groups.js
@@ -172,19 +172,19 @@
 			newGroupButtonSelector: '#newgroupbtn',
 			groupSettingsSelector: '#groupsettingsbtn',
 
-			newGroupAction: 'sproutforms/groups/save-group',
-			newGroupOnSuccessUrlBase: 'sproutforms/forms/',
+			newGroupAction: 'sprout-forms/groups/save-group',
+			newGroupOnSuccessUrlBase: 'sprout-forms/forms/',
 			newGroupOnErrorMessage: Craft.t('sproutforms','Could not create the group:'),
 
-			renameGroupAction: 'sproutforms/groups/save-group',
+			renameGroupAction: 'sprout-forms/groups/save-group',
 			renameGroupOnSuccessMessage: Craft.t('sproutforms','Group renamed.'),
 			renameGroupOnErrorMessage: Craft.t('sproutforms','Could not rename the group:'),
 
 			promptForGroupNameMessage: Craft.t('sproutforms','What do you want to name your group?'),
 
 			deleteGroupConfirmMessage: Craft.t('app','Are you sure you want to delete this group and all its fields?'),
-			deleteGroupAction: 'sproutforms/groups/delete-group',
-			deleteGroupOnSuccessUrl: 'sproutforms/forms/',
+			deleteGroupAction: 'sprout-forms/groups/delete-group',
+			deleteGroupOnSuccessUrl: 'sprout-forms/forms/',
 			deleteGroupOnErrorMessage: Craft.t('sproutforms','Could not delete the group.'),
 		});
 	});
