cf6ad7bef58cef1496b95958ec755e4d227a1cc8
Andre Lopez <andrelopezd@gmail.com>
2018-03-14 16:32:23 -0400

    Adds initial migrations for Craft2. #
    migrations for plaintext, dropdown, checkboxes, multiselect, radiobuttons, number Fields

---
+++ src/migrations/m180314_161524_sproutforms_checkboxes_fields.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace barrelstrength\sproutforms\migrations;
+
+use craft\db\Migration;
+use craft\db\Query;
+use barrelstrength\sproutforms\integrations\sproutforms\fields\Checkboxes;
+use craft\fields\Checkboxes as CraftCheckboxes;
+
+/**
+ * m180314_161524_sproutforms_checkboxes_fields migration.
+ */
+class m180314_161524_sproutforms_checkboxes_fields extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function safeUp()
+    {
+        $checkboxesFields = (new Query())
+            ->select(['id', 'handle', 'settings'])
+            ->from(['{{%fields}}'])
+            ->where(['type' => CraftCheckboxes::class])
+            ->andWhere('context LIKE "%sproutForms:%"')
+            ->all();
+
+        foreach ($checkboxesFields as $checkboxesField) {
+            $this->update('{{%fields}}', ['type' => Checkboxes::class], ['id' => $checkboxesField['id']], [], false);
+        }
+
+        return true;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function safeDown()
+    {
+        echo "m180314_161524_sproutforms_checkboxes_fields cannot be reverted.\n";
+        return false;
+    }
+}
