4afc56f51aeaf2e56b91e9c6c50125fb9ec1c014
Andre Lopez <andrelopezd@gmail.com>
2018-03-15 16:30:07 -0400

    Adds Old Sprout Fields migrations. #43

---
+++ src/migrations/m180314_161532_sproutforms_hidden_fields.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace barrelstrength\sproutforms\migrations;
+
+use craft\db\Migration;
+use craft\db\Query;
+use barrelstrength\sproutforms\integrations\sproutforms\fields\Hidden;
+
+/**
+ * m180314_161532_sproutforms_hidden_fields migration.
+ */
+class m180314_161532_sproutforms_hidden_fields extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function safeUp()
+    {
+        $fields = (new Query())
+            ->select(['id', 'handle', 'settings'])
+            ->from(['{{%fields}}'])
+            ->where(['type' => 'SproutFields_Hidden'])
+            ->andWhere('context LIKE "%sproutForms:%"')
+            ->all();
+
+        foreach ($fields as $field) {
+            $this->update('{{%fields}}', ['type' => Hidden::class], ['id' => $field['id']], [], false);
+        }
+
+        return true;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function safeDown()
+    {
+        echo "m180314_161532_sproutforms_hidden_fields cannot be reverted.\n";
+        return false;
+    }
+}
---
+++ src/migrations/m180314_161533_sproutforms_invisible_fields.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace barrelstrength\sproutforms\migrations;
+
+use craft\db\Migration;
+use craft\db\Query;
+use barrelstrength\sproutforms\integrations\sproutforms\fields\Invisible;
+
+/**
+ * m180314_161533_sproutforms_invisible_fields migration.
+ */
+class m180314_161533_sproutforms_invisible_fields extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function safeUp()
+    {
+        $fields = (new Query())
+            ->select(['id', 'handle', 'settings'])
+            ->from(['{{%fields}}'])
+            ->where(['type' => 'SproutFields_Invisible'])
+            ->andWhere('context LIKE "%sproutForms:%"')
+            ->all();
+
+        foreach ($fields as $field) {
+            $this->update('{{%fields}}', ['type' => Invisible::class], ['id' => $field['id']], [], false);
+        }
+
+        return true;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function safeDown()
+    {
+        echo "m180314_161533_sproutforms_invisible_fields cannot be reverted.\n";
+        return false;
+    }
+}
