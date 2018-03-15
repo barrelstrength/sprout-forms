669a8744362bce1455aea7595844cc9fbdf7180f
Ben Parizek <ben@barrelstrengthdesign.com>
2018-03-15 04:44:59 -0400

    Fixes asset path

--- src/integrations/sproutforms/fields/FileUpload.php
+++ src/integrations/sproutforms/fields/FileUpload.php
@@ -78,7 +78,7 @@ class FileUpload extends SproutBaseRelationField
         parent::init();
         $this->allowLargeThumbsView = true;
         $this->settingsTemplate = 'sprout-forms/_components/fields/fileupload/settings';
-        $this->inputTemplate = '_components/fields/assets/input';
+        $this->inputTemplate = '_components/fieldtypes/Assets/input';
         $this->inputJsClass = 'Craft.AssetSelectInput';
     }
 
