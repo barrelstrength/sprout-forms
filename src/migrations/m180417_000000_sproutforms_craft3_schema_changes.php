9f16592eeaba3004da5d33744e2a72c297a2c5fb
Ben Parizek <ben@barrelstrengthdesign.com>
2018-04-17 22:09:44 -0400

    Adds Sprout Reports schema change migration

---
+++ src/migrations/m180417_000000_sproutforms_craft3_schema_changes.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace barrelstrength\sproutforms\migrations;
+
+use barrelstrength\sproutbase\migrations\sproutreports\m180307_042132_craft3_schema_changes;
+use craft\db\Migration;
+
+/**
+ * m180412_000000_sproutforms_sprout_email migration.
+ */
+class m180417_000000_sproutforms_craft3_schema_changes extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function safeUp()
+    {
+        $migration = new m180307_042132_craft3_schema_changes();
+
+        ob_start();
+        $migration->safeUp();
+        ob_end_clean();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function safeDown()
+    {
+        echo "m180417_000000_sproutforms_craft3_schema_changes cannot be reverted.\n";
+        return false;
+    }
+}
