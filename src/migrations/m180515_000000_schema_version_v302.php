9ef89d6cad1d1653eba92e4d6393198e1f3d53ef
Ben Parizek <ben@barrelstrengthdesign.com>
2018-05-16 23:33:32 -0400

    Adds Notification options and Report Element type migrations

--- src/migrations/m180515_000000_schema_version_v302.php
+++ src/migrations/m180515_000000_schema_version_v302.php
@@ -4,7 +4,9 @@ namespace barrelstrength\sproutforms\migrations;
 
 use barrelstrength\sproutbase\app\email\migrations\m180515_000000_rename_notification_pluginId_column;
 use barrelstrength\sproutbase\app\email\migrations\m180515_000001_update_notification_element_types;
+use barrelstrength\sproutbase\app\email\migrations\m180515_000002_rename_notification_options_column;
 use barrelstrength\sproutbase\app\reports\migrations\m180515_000001_rename_datasources_pluginId_column;
+use barrelstrength\sproutbase\app\reports\migrations\m180515_000002_update_report_element_types;
 use craft\db\Migration;
 
 /**
@@ -28,11 +30,23 @@ class m180515_000000_schema_version_v302 extends Migration
         ob_start();
         $notificationPluginIdMigration->safeUp();
         ob_end_clean();
+        
+        $notificationOptionsMigration = new m180515_000002_rename_notification_options_column();
+
+        ob_start();
+        $notificationOptionsMigration->safeUp();
+        ob_end_clean();
 
         $notificationElementTypeMigration = new m180515_000001_update_notification_element_types();
 
         ob_start();
         $notificationElementTypeMigration->safeUp();
+        ob_end_clean();
+
+        $reportElementTypeMigration = new m180515_000002_update_report_element_types();
+
+        ob_start();
+        $reportElementTypeMigration->safeUp();
         ob_end_clean();
 
         return true;
