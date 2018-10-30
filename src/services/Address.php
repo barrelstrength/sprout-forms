add3a8496a1dcbd65886f85c3651036a55655c3a
Dale Ramirez <dalefirstpage@gmail.com>
2018-10-30 19:10:38 +0800

    - Created AddressController for address field front end request that also allows non logged in users - Overriden countryInput method on Address service to change the actionUrl for front end address - Added fieldClass variable for input and select field and changed name to nameValue - Modified vanilla js to accept json request and parses json result that changes the input based on the country selected.

--- src/services/Address.php
+++ src/services/Address.php
@@ -4,6 +4,7 @@ namespace barrelstrength\sproutforms\services;
 
 use barrelstrength\sproutbase\app\fields\helpers\AddressHelper;
 use Craft;
+use craft\helpers\UrlHelper;
 
 class Address extends AddressHelper
 {
@@ -12,5 +13,26 @@ class Address extends AddressHelper
         $html = Craft::$app->view->renderTemplate('address/_components/'.$template, $params);
 
         return $html;
+    }
+
+    public function countryInput($hidden = null)
+    {
+        $countries = $this->getCountries();
+
+        return $this->renderTemplates(
+            'select',
+            [
+                'class' => 'sproutaddress-country-select',
+                'label' => $this->renderHeading('Country'),
+                'name' => $this->name.'[countryCode]',
+                'inputName' => 'countryCode',
+                'options' => $countries,
+                'value' => $this->countryCode ?? $this->defaultCountryCode(),
+                'nameValue' => $this->name,
+                'hidden' => $hidden,
+                'actionUrl' => UrlHelper::actionUrl('sprout-forms/address/change-form'),
+                'addressInfo' => $this->addressModel
+            ]
+        );
     }
 }
