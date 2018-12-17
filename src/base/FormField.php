d99bdacdca9522f2a698f644dafede269022b112
Andre L <andrelopezd@gmail.com>
2018-12-17 17:35:21 -0400

    Adds invisible field value

--- src/base/FormField.php
+++ src/base/FormField.php
@@ -13,6 +13,8 @@ use craft\base\Field;
  */
 abstract class FormField extends Field
 {
+    CONST ADD_VARIABLES_SESSION_KEY = 'sprout-forms-field-variables';
+
     /**
      * @var array
      */
@@ -28,22 +30,27 @@ abstract class FormField extends Field
      */
     protected $originalTemplatesPath;
 
+
     /**
+     *
      * Allows a user to add variables to an object that can be parsed by fields
      *
      * @example
-     * {% do craft.sproutForms.addFieldVariables({ entry: entry }) %}
+     * {% do craft.sproutForms.addFieldVariables({ entryTitle: entry.title }) %}
      * {{ craft.sproutForms.displayForm('contact') }}
      *
      * @param array $variables
+     * @throws \craft\errors\MissingComponentException
      */
     public static function addFieldVariables(array $variables)
     {
         static::$fieldVariables = array_merge(static::$fieldVariables, $variables);
+        //Craft::$app->getSession()->set(self::ADD_VARIABLES_SESSION_KEY, static::$fieldVariables);
     }
 
     /**
-     * @return array
+     * @return mixed
+     * @throws \craft\errors\MissingComponentException
      */
     public static function getFieldVariables()
     {
