9e69437e89f75a28c3df582dc36139f3bd654593
Andre L <andrelopezd@gmail.com>
2018-12-17 13:11:21 -0400

    Adds migrations  for Sprout Forms and Sprout entries craft2 fields. #204

---
+++ src/migrations/m181217_000000_update_sproutforms_field_types.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace barrelstrength\sproutforms\migrations;
+
+use craft\db\Migration;
+
+/**
+ * m181217_000000_update_sproutforms_field_types migration.
+ */
+class m181217_000000_update_sproutforms_field_types extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function safeUp()
+    {
+        $fieldClasses = [
+            0 => [
+                'oldType' => 'SproutForms_Forms',
+                'newType' => 'barrelstrength\sproutforms\fields\Forms'
+            ],
+            1 => [
+                'oldType' => 'SproutForms_Entry',
+                'newType' => 'barrelstrength\sproutforms\fields\Entries'
+            ]
+        ];
+
+        foreach ($fieldClasses as $fieldClass) {
+            $this->update('{{%fields}}', [
+                'type' => $fieldClass['newType']], ['type' => $fieldClass['oldType']], [], false);
+        }
+
+        return true;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function safeDown()
+    {
+        echo "m181217_000000_update_sproutforms_field_types cannot be reverted.\n";
+        return false;
+    }
+}
