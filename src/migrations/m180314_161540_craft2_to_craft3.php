83e1a9a1dc27f2702f7009f15f7633ddaca2012a
Andre L <andrelopezd@gmail.com>
2019-02-06 15:59:30 -0400

    Fixes Undefined index notificationSubject on migration . #226

--- src/migrations/m180314_161540_craft2_to_craft3.php
+++ src/migrations/m180314_161540_craft2_to_craft3.php
@@ -127,31 +127,32 @@ class m180314_161540_craft2_to_craft3 extends Migration
             ->all();
 
         foreach ($forms as $form) {
+            if (isset($form['notificationSubject']) && isset($form['notificationSenderName']) && isset($form['notificationSenderEmail'])){
+                if ($form['notificationSubject'] && $form['notificationSenderName'] && $form['notificationSenderEmail']){
 
-            if ($form['notificationSubject'] && $form['notificationSenderName'] && $form['notificationSenderEmail']){
+                    $notificationEmail = new NotificationEmail();
 
-                $notificationEmail = new NotificationEmail();
+                    $settings = [
+                        'whenNew' => '1',
+                        'formIds' => [
+                            $form['id']
+                        ]
+                    ];
 
-                $settings = [
-                    'whenNew' => '1',
-                    'formIds' => [
-                        $form['id']
-                    ]
-                ];
+                    $notificationEmail->subjectLine = $form['notificationSubject'];
+                    $notificationEmail->fromName = $form['notificationSenderName'];
+                    $notificationEmail->fromEmail = $form['notificationSenderEmail'];
+                    $notificationEmail->replyToEmail = $form['notificationReplyToEmail'];
+                    $notificationEmail->recipients = $form['notificationRecipients'];
+                    $notificationEmail->title = $notificationEmail->subjectLine;
+                    $notificationEmail->pluginHandle = 'sprout-forms';
+                    $notificationEmail->enableFileAttachments = $form['enableFileAttachments'];
+                    $notificationEmail->settings = Json::encode($settings);
+                    $notificationEmail->enabled = $form['notificationEnabled'] ?? 0;
+                    $notificationEmail->eventId = 'barrelstrength\sproutforms\integrations\sproutemail\events\notificationevents\SaveEntryEvent';
 
-                $notificationEmail->subjectLine = $form['notificationSubject'];
-                $notificationEmail->fromName = $form['notificationSenderName'];
-                $notificationEmail->fromEmail = $form['notificationSenderEmail'];
-                $notificationEmail->replyToEmail = $form['notificationReplyToEmail'];
-                $notificationEmail->recipients = $form['notificationRecipients'];
-                $notificationEmail->title = $notificationEmail->subjectLine;
-                $notificationEmail->pluginHandle = 'sprout-forms';
-                $notificationEmail->enableFileAttachments = $form['enableFileAttachments'];
-                $notificationEmail->settings = Json::encode($settings);
-                $notificationEmail->enabled = $form['notificationEnabled'] ?? 0;
-                $notificationEmail->eventId = 'barrelstrength\sproutforms\integrations\sproutemail\events\notificationevents\SaveEntryEvent';
-
-                SproutBase::$app->notifications->saveNotification($notificationEmail);
+                    SproutBase::$app->notifications->saveNotification($notificationEmail);
+                }
             }
         }
 
