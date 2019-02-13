0c102727788460c5aeb3385e0535dcba89fe1277
Andre L <andrelopezd@gmail.com>
2019-02-13 19:40:21 -0400

    Adds Sprout Email migration

---
+++ src/migrations/m190212_000003_update_email_template_id_sproutforms.php
@@ -0,0 +1,35 @@
+<?php /** @noinspection ClassConstantCanBeUsedInspection */
+
+namespace barrelstrength\sproutforms\migrations;
+
+use barrelstrength\sproutbaseemail\migrations\m190212_000003_update_email_template_id;
+use craft\db\Migration;
+
+/**
+ * m190212_000003_update_email_template_id_sproutforms migration.
+ */
+class m190212_000003_update_email_template_id_sproutforms extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function safeUp(): bool
+    {
+        $migration = new m190212_000003_update_email_template_id();
+
+        ob_start();
+        $migration->safeUp();
+        ob_end_clean();
+
+        return true;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function safeDown(): bool
+    {
+        echo "m190212_000003_update_email_template_id_sproutforms cannot be reverted.\n";
+        return false;
+    }
+}
