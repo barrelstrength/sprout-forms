6dd93a690c08134c49af3a67e74f6834e081701f
Andre L <andrelopezd@gmail.com>
2019-04-09 17:30:37 -0400

    Updates migrations

--- src/migrations/m181120_000000_add_integrations_table.php
+++ src/migrations/m181120_000000_add_integrations_table.php
@@ -21,6 +21,7 @@ class m181120_000000_add_integrations_table extends Migration
             'type' => $this->string()->notNull(),
             'settings' => $this->text(),
             'enabled' => $this->boolean()->defaultValue(true),
+            'addErrorOnSubmit' => $this->boolean()->defaultValue(true),
             'dateCreated' => $this->dateTime()->notNull(),
             'dateUpdated' => $this->dateTime()->notNull(),
             'uid' => $this->uid(),
