fa8e89fa3a460881b5579bd8c8a90b5cb85c1c73
Andre L <andrelopezd@gmail.com>
2019-06-28 13:49:27 -0400

    Adds sprout reports migration

--- src/migrations/m190628_000000_add_default_to_pro_and_fix_data_sources.php
+++ src/migrations/m190628_000000_add_default_to_pro_and_fix_data_sources.php
@@ -2,14 +2,15 @@
 
 namespace barrelstrength\sproutforms\migrations;
 
+use barrelstrength\sproutbasereports\migrations\m190628_000000_fix_data_sources;
 use Craft;
 use craft\db\Migration;
 use craft\services\Plugins;
 
 /**
- * m190628_000000_add_default_to_pro migration.
+ * m190628_000000_add_default_to_pro_and_fix_data_sources migration.
  */
-class m190628_000000_add_default_to_pro extends Migration
+class m190628_000000_add_default_to_pro_and_fix_data_sources extends Migration
 {
     /**
      * @return bool
@@ -21,6 +22,12 @@ class m190628_000000_add_default_to_pro extends Migration
         $pluginHandle = 'sprout-forms';
         $projectConfig->set(Plugins::CONFIG_PLUGINS_KEY.'.'.$pluginHandle.'.edition', 'pro');
 
+        $migration = new m190628_000000_fix_data_sources();
+
+        ob_start();
+        $migration->safeUp();
+        ob_end_clean();
+
         return true;
     }
 
@@ -29,7 +36,7 @@ class m190628_000000_add_default_to_pro extends Migration
      */
     public function safeDown(): bool
     {
-        echo "m190628_000000_add_default_to_pro cannot be reverted.\n";
+        echo "m190628_000000_add_default_to_pro_and_fix_data_sources cannot be reverted.\n";
         return false;
     }
 }
