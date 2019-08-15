98bafa249dd1592f4a91b68be27827f419898ee3
Ben Parizek <ben@barrelstrengthdesign.com>
2019-08-15 12:51:55 -0400

    Adds Project Config check in migration
    - https://github.com/barrelstrength/craft-sprout-forms/issues/312

--- src/migrations/m190410_000000_add_payload_forwarding_to_integration.php
+++ src/migrations/m190410_000000_add_payload_forwarding_to_integration.php
@@ -69,12 +69,19 @@ class m190410_000000_add_payload_forwarding_to_integration extends Migration
             }
         }
 
-        // Add enableIntegrationsPerFormBasis setting
         $projectConfig = Craft::$app->getProjectConfig();
         $pluginHandle = 'sprout-forms';
-        $currentSettings = $projectConfig->get(Plugins::CONFIG_PLUGINS_KEY.'.'.$pluginHandle.'.settings');
-        $currentSettings['enableIntegrationsPerFormBasis'] = $enableIntegrations;
-        $projectConfig->set(Plugins::CONFIG_PLUGINS_KEY.'.'.$pluginHandle.'.settings', $currentSettings);
+
+        // Don't make the same config changes twice
+        $schemaVersion = Craft::$app->projectConfig
+            ->get('plugins.'.$pluginHandle.'.schemaVersion', true);
+
+        if (version_compare($schemaVersion, '3.0.20', '<')) {
+            // Add enableIntegrationsPerFormBasis setting
+            $currentSettings = $projectConfig->get(Plugins::CONFIG_PLUGINS_KEY.'.'.$pluginHandle.'.settings');
+            $currentSettings['enableIntegrationsPerFormBasis'] = $enableIntegrations;
+            $projectConfig->set(Plugins::CONFIG_PLUGINS_KEY.'.'.$pluginHandle.'.settings', $currentSettings);
+        }
 
         // Cleanup
         if ($this->db->columnExists('{{%sproutforms_forms}}', 'submitAction')) {
