50ea812c5aac2d3f176278baa2bc24b1180ac65d
Ben Parizek <ben@barrelstrengthdesign.com>
2019-09-26 12:13:44 -0400

    Fixed bug where Entry Status failed to save new Status when using Postgres

--- src/services/Entries.php
+++ src/services/Entries.php
@@ -117,6 +117,8 @@ class Entries extends Component
      */
     public function saveEntryStatus(EntryStatus $entryStatus): bool
     {
+        $isNew = !$entryStatus->id;
+
         $record = new EntryStatusRecord();
 
         if ($entryStatus->id) {
@@ -127,7 +129,13 @@ class Entries extends Component
             }
         }
 
-        $record->setAttributes($entryStatus->getAttributes(), false);
+        $attributes = $entryStatus->getAttributes();
+
+        if ($isNew) {
+            unset($attributes['id']);
+        }
+
+        $record->setAttributes($attributes, false);
 
         $record->sortOrder = $entryStatus->sortOrder ?: 999;
 
