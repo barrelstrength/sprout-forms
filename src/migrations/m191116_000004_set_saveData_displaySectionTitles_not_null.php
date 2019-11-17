706e2e65fcbed9ccfcd1a6ee0daa4005682e7da4
Ben Parizek <ben@barrelstrengthdesign.com>
2019-11-16 20:42:58 -0500

    Update saveData and displaySectionTitles to not null
    - Update Form Save Data settings from null to true
    - Update Display Page Titles from null to false

---
+++ src/migrations/m191116_000004_set_saveData_displaySectionTitles_not_null.php
@@ -0,0 +1,67 @@
+<?php /** @noinspection ClassConstantCanBeUsedInspection */
+
+namespace barrelstrength\sproutforms\migrations;
+
+use craft\config\DbConfig;
+use craft\db\Migration;
+use Craft;
+use craft\db\Query;
+
+/**
+ * m191116_000004_set_saveData_displaySectionTitles_not_null migration.
+ */
+class m191116_000004_set_saveData_displaySectionTitles_not_null extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function safeUp(): bool
+    {
+        $table = '{{%sproutforms_forms}}';
+
+        $forms = (new Query())
+            ->select([
+                'id',
+                'displaySectionTitles',
+                'saveData'
+            ])
+            ->from([$table])
+            ->all();
+
+        foreach ($forms as $form) {
+            $this->update($table, [
+                'displaySectionTitles' => false
+            ], [
+                'displaySectionTitles' => null
+            ], [], false);
+
+            $this->update($table, [
+                'saveData' => true
+            ], [
+                'saveData' => null
+            ], [], false);
+        }
+
+        // https://github.com/yiisoft/yii2/issues/4492
+        if (Craft::$app->getDb()->getDriverName() === DbConfig::DRIVER_PGSQL) {
+            $this->alterColumn($table, 'displaySectionTitles', 'SET NOT NULL');
+            $this->alterColumn($table, 'displaySectionTitles', 'SET DEFAULT FALSE');
+            $this->alterColumn($table, 'saveData', 'SET NOT NULL');
+            $this->alterColumn($table, 'saveData', 'SET DEFAULT TRUE');
+        } else {
+            $this->alterColumn($table, 'displaySectionTitles', $this->boolean()->defaultValue(false)->notNull());
+            $this->alterColumn($table, 'saveData', $this->boolean()->defaultValue(false)->notNull());
+        }
+
+        return true;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function safeDown(): bool
+    {
+        echo "m191116_000004_set_saveData_displaySectionTitles_not_null cannot be reverted.\n";
+        return false;
+    }
+}
