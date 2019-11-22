437330def92d9c5c20d8547812ac5bc10d09a78b
Ben Parizek <ben@barrelstrengthdesign.com>
2019-11-22 14:51:29 -0500

    Adds back support for updating Title Format after changing field handle

--- src/controllers/FieldsController.php
+++ src/controllers/FieldsController.php
@@ -322,7 +322,7 @@ class FieldsController extends BaseController
         // field layout of our Form Element
         if ($response) {
             Craft::info('Field Saved', __METHOD__);
-
+            SproutForms::$app->forms->saveForm($form);
             return $this->returnJson(true, $field, $form, $tabName, $tabId);
         }
 
