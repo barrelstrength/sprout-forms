a5be20e7c8f37472f23ef219ebab6c939adb7199
Ben Parizek <ben.parizek@gmail.com>
2020-03-15 17:15:52 -0400

    Adds top level Relations Field classes using FormFieldTrait
    - Addresses multiple inheritance of Craft Relation fields by using FormFieldTrait and extending Craft Relations Fields directly
    - Removes BaseRelationFormField

---
+++ src/fields/formfields/BaseAssetsFormField.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @link      https://sprout.barrelstrengthdesign.com
+ * @copyright Copyright (c) Barrel Strength Design LLC
+ * @license   https://craftcms.github.io/license
+ */
+
+namespace barrelstrength\sproutforms\fields\formfields;
+
+use barrelstrength\sproutforms\base\FormFieldTrait;
+use craft\fields\Assets as CraftAssetsField;
+
+abstract class BaseAssetsFormField extends CraftAssetsField
+{
+    use FormFieldTrait;
+}
---
+++ src/fields/formfields/BaseUsersFormField.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @link      https://sprout.barrelstrengthdesign.com
+ * @copyright Copyright (c) Barrel Strength Design LLC
+ * @license   https://craftcms.github.io/license
+ */
+
+namespace barrelstrength\sproutforms\fields\formfields;
+
+use barrelstrength\sproutforms\base\FormFieldTrait;
+use craft\fields\Users as CraftUsersField;
+
+abstract class BaseUsersFormField extends CraftUsersField
+{
+    use FormFieldTrait;
+
+    /**
+     * @var string Template to use for settings rendering
+     */
+    protected $settingsTemplate = 'sprout-forms/_components/fields/formfields/elementfieldsettings';
+}
