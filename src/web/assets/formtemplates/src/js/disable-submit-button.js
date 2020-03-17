1f95e2d6e4729445fb1ab87901cf1986376df6d0
Ben Parizek <ben.parizek@gmail.com>
2020-03-17 12:26:07 -0400

    Update DisableSubmitButton logic
    - Make sure submit buttons are enabled if more edits are made to a form: https://github.com/barrelstrength/craft-sprout-forms/issues/411

--- src/web/assets/formtemplates/src/js/disable-submit-button.js
+++ src/web/assets/formtemplates/src/js/disable-submit-button.js
@@ -10,17 +10,33 @@ class SproutFormsDisableSubmitButton {
   constructor(formId) {
     this.formId = formId;
     this.form = document.getElementById(this.formId);
+    this.submitButtons = this.form.querySelectorAll('[type="submit"]');
 
     this.setDuplicateSubmissionEventListener();
+    this.setResetDuplicateSubmissionEventListener();
   }
 
+  // Mark all submit buttons as disabled once a user submits the form
   setDuplicateSubmissionEventListener() {
+    let self = this;
     this.form.addEventListener('submit', function() {
-      let buttons = this.querySelectorAll('[type="submit"]');
-      buttons.forEach(function(button) {
+      self.submitButtons.forEach(function(button) {
         button.setAttribute('disabled', 'disabled');
       });
     }, false);
+  }
+
+  // If any inputs change, make sure all submit buttons are not disabled
+  setResetDuplicateSubmissionEventListener() {
+    let self = this;
+    let inputs = this.form.querySelectorAll('input, select, option, textarea, button, datalist, output');
+    inputs.forEach(function(input) {
+      input.addEventListener('input', function() {
+        self.submitButtons.forEach(function(button) {
+          button.removeAttribute('disabled');
+        });
+      }, false);
+    });
   }
 }
 
