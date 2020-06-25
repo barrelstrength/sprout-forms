e3d95d55cf465c28424411b8d09654dc1c86ff71
Ben Parizek <ben.parizek@gmail.com>
2020-06-25 10:04:08 -0400

    Update config and loading

--- src/SproutForms.php
+++ src/SproutForms.php
@@ -14,7 +14,7 @@ use barrelstrength\sproutbase\app\forms\base\Captcha;
 use barrelstrength\sproutbase\app\forms\captchas\DuplicateCaptcha;
 use barrelstrength\sproutbase\app\forms\captchas\HoneypotCaptcha;
 use barrelstrength\sproutbase\app\forms\captchas\JavascriptCaptcha;
-use barrelstrength\sproutbase\app\forms\controllers\EntriesController;
+use barrelstrength\sproutbase\app\forms\controllers\FormEntriesController;
 use barrelstrength\sproutbase\app\forms\elements\Entry as EntryElement;
 use barrelstrength\sproutbase\app\forms\events\OnBeforeValidateEntryEvent;
 use barrelstrength\sproutbase\app\forms\events\OnSaveEntryEvent;
@@ -22,7 +22,6 @@ use barrelstrength\sproutbase\app\forms\events\RegisterFieldsEvent;
 use barrelstrength\sproutbase\app\forms\fields\Entries as FormEntriesField;
 use barrelstrength\sproutbase\app\forms\fields\Forms as FormsField;
 use barrelstrength\sproutbase\app\forms\formtemplates\AccessibleTemplates;
-use barrelstrength\sproutbase\app\forms\formtemplates\BasicTemplates;
 use barrelstrength\sproutbase\app\forms\integrations\sproutemail\emailtemplates\basic\BasicSproutFormsNotification;
 use barrelstrength\sproutbase\app\forms\integrations\sproutemail\events\notificationevents\SaveEntryEvent;
 use barrelstrength\sproutbase\app\forms\integrations\sproutreports\datasources\EntriesDataSource;
@@ -30,19 +29,20 @@ use barrelstrength\sproutbase\app\forms\integrations\sproutreports\datasources\I
 use barrelstrength\sproutbase\app\forms\integrations\sproutreports\datasources\SpamLogDataSource;
 use barrelstrength\sproutbase\app\forms\integrationtypes\CustomEndpoint;
 use barrelstrength\sproutbase\app\forms\integrationtypes\EntryElementIntegration;
-use barrelstrength\sproutbase\app\forms\services\Entries;
-use barrelstrength\sproutbase\app\forms\services\Fields as SproutFormsFields;
+use barrelstrength\sproutbase\app\forms\services\FormCaptchas;
+use barrelstrength\sproutbase\app\forms\services\FormEntries;
+use barrelstrength\sproutbase\app\forms\services\FormFields as SproutFormsFields;
 use barrelstrength\sproutbase\app\forms\services\Forms;
-use barrelstrength\sproutbase\app\forms\services\Integrations;
+use barrelstrength\sproutbase\app\forms\services\FormIntegrations;
+use barrelstrength\sproutbase\app\forms\services\FormTemplates;
 use barrelstrength\sproutbase\app\forms\widgets\RecentEntries;
-use barrelstrength\sproutbase\app\reports\base\DataSource;
 use barrelstrength\sproutbase\app\reports\services\DataSources;
-use barrelstrength\sproutbase\config\base\EditionsInterface;
 use barrelstrength\sproutbase\config\base\SproutBasePlugin;
-use barrelstrength\sproutbase\config\configs\EmailConfig;
+use barrelstrength\sproutbase\config\configs\ControlPanelConfig;
+use barrelstrength\sproutbase\config\configs\EmailPreviewConfig;
 use barrelstrength\sproutbase\config\configs\FieldsConfig;
 use barrelstrength\sproutbase\config\configs\FormsConfig;
-use barrelstrength\sproutbase\config\configs\ControlPanelConfig;
+use barrelstrength\sproutbase\config\configs\NotificationsConfig;
 use barrelstrength\sproutbase\config\configs\ReportsConfig;
 use barrelstrength\sproutbase\SproutBase;
 use barrelstrength\sproutbase\SproutBaseHelper;
@@ -51,16 +51,11 @@ use craft\events\RegisterComponentTypesEvent;
 use craft\helpers\UrlHelper;
 use craft\services\Dashboard;
 use craft\services\Fields;
-use craft\services\Plugins;
-use craft\web\twig\variables\CraftVariable;
 use Exception;
 use Throwable;
-use yii\base\ErrorException;
 use yii\base\Event;
-use yii\base\NotSupportedException;
-use yii\web\ServerErrorHttpException;
 
-class SproutForms extends SproutBasePlugin implements EditionsInterface
+class SproutForms extends SproutBasePlugin
 {
     const EDITION_LITE = 'lite';
     const EDITION_PRO = 'pro';
@@ -89,8 +84,8 @@ class SproutForms extends SproutBasePlugin implements EditionsInterface
     public static function getSproutConfigs(): array
     {
         return [
-            ControlPanelConfig::class,
-            EmailConfig::class,
+            NotificationsConfig::class,
+            EmailPreviewConfig::class,
             FieldsConfig::class,
             FormsConfig::class,
             ReportsConfig::class
@@ -108,20 +103,13 @@ class SproutForms extends SproutBasePlugin implements EditionsInterface
         });
 
         Event::on(SproutFormsFields::class, SproutFormsFields::EVENT_REGISTER_FIELDS, static function(RegisterFieldsEvent $event) {
-            $fieldsByGroup = SproutBase::$app->fields->getRegisteredFieldsByGroup();
+            $fieldsByGroup = SproutBase::$app->formFields->getRegisteredFieldsByGroup();
 
             foreach ($fieldsByGroup as $group) {
                 foreach ($group as $field) {
                     $event->fields[] = new $field;
                 }
             }
-        });
-
-        // Register DataSources for sproutReports plugin integration
-        Event::on(DataSources::class, DataSources::EVENT_REGISTER_DATA_SOURCES, static function(RegisterComponentTypesEvent $event) {
-            $event->types[] = EntriesDataSource::class;
-            $event->types[] = IntegrationLogDataSource::class;
-            $event->types[] = SpamLogDataSource::class;
         });
 
 //        $this->setComponents([
@@ -141,13 +129,13 @@ class SproutForms extends SproutBasePlugin implements EditionsInterface
             $event->events[] = SaveEntryEvent::class;
         });
 
-        Event::on(Forms::class, Forms::EVENT_REGISTER_CAPTCHAS, static function(RegisterComponentTypesEvent $event) {
+        Event::on(FormCaptchas::class, FormCaptchas::EVENT_REGISTER_CAPTCHAS, static function(RegisterComponentTypesEvent $event) {
             $event->types[] = DuplicateCaptcha::class;
             $event->types[] = JavascriptCaptcha::class;
             $event->types[] = HoneypotCaptcha::class;
         });
 
-        Event::on(EntriesController::class, EntriesController::EVENT_BEFORE_VALIDATE, static function(OnBeforeValidateEntryEvent $event) {
+        Event::on(FormEntriesController::class, FormEntriesController::EVENT_BEFORE_VALIDATE, static function(OnBeforeValidateEntryEvent $event) {
 
             if (Craft::$app->getRequest()->getIsSiteRequest()) {
                 $enableCaptchas = (int)$event->form->enableCaptchas;
@@ -158,7 +146,7 @@ class SproutForms extends SproutBasePlugin implements EditionsInterface
                 }
 
                 /** @var Captcha[] $captchas */
-                $captchas = SproutBase::$app->forms->getAllEnabledCaptchas();
+                $captchas = SproutBase::$app->formCaptchas->getAllEnabledCaptchas();
 
                 foreach ($captchas as $captcha) {
                     $captcha->verifySubmission($event);
@@ -167,21 +155,20 @@ class SproutForms extends SproutBasePlugin implements EditionsInterface
             }
         }, null, false);
 
-        Event::on(Entries::class, EntryElement::EVENT_AFTER_SAVE, static function(OnSaveEntryEvent $event) {
-            SproutBase::$app->integrations->runFormIntegrations($event->entry);
+        Event::on(FormEntries::class, EntryElement::EVENT_AFTER_SAVE, static function(OnSaveEntryEvent $event) {
+            SproutBase::$app->formIntegrations->runFormIntegrations($event->entry);
         });
 
         Craft::$app->view->hook('sproutForms.modifyForm', static function(array &$context) {
             return SproutBase::$app->forms->handleModifyFormHook($context);
         });
 
-        Event::on(Integrations::class, Integrations::EVENT_REGISTER_INTEGRATIONS, static function(RegisterComponentTypesEvent $event) {
+        Event::on(FormIntegrations::class, FormIntegrations::EVENT_REGISTER_INTEGRATIONS, static function(RegisterComponentTypesEvent $event) {
             $event->types[] = CustomEndpoint::class;
             $event->types[] = EntryElementIntegration::class;
         });
 
-        Event::on(Forms::class, Forms::EVENT_REGISTER_FORM_TEMPLATES, static function(RegisterComponentTypesEvent $event) {
-            $event->types[] = BasicTemplates::class;
+        Event::on(FormTemplates::class, FormTemplates::EVENT_REGISTER_FORM_TEMPLATES, static function(RegisterComponentTypesEvent $event) {
             $event->types[] = AccessibleTemplates::class;
         });
 
@@ -189,18 +176,6 @@ class SproutForms extends SproutBasePlugin implements EditionsInterface
         Event::on(EmailTemplates::class, EmailTemplates::EVENT_REGISTER_EMAIL_TEMPLATES, static function(RegisterComponentTypesEvent $event) {
             $event->types[] = BasicSproutFormsNotification::class;
         });
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function getUpgradeUrl()
-    {
-        if (!SproutBase::$app->config->isEdition('sprout-forms', self::EDITION_PRO)) {
-            return UrlHelper::cpUrl('sprout-forms/upgrade');
-        }
-
-        return null;
     }
 
     /**
@@ -219,38 +194,15 @@ class SproutForms extends SproutBasePlugin implements EditionsInterface
         return true;
     }
 
-    /**
-     * @throws ErrorException
-     * @throws \yii\base\Exception
-     * @throws NotSupportedException
-     * @throws ServerErrorHttpException
-     */
     protected function afterInstall()
     {
-        // Add DataSource integrations so users don't have to install them manually
-        $dataSourceTypes = [
-            EntriesDataSource::class,
-            IntegrationLogDataSource::class,
-            SpamLogDataSource::class
-        ];
-
-        // @todo research why the plugin is not enabled after install
-        $projectConfig = Craft::$app->getProjectConfig();
-        $pluginHandle = 'sprout-forms';
-        $projectConfig->set(Plugins::CONFIG_PLUGINS_KEY.'.'.$pluginHandle.'.enabled', true);
-
-        foreach ($dataSourceTypes as $dataSourceClass) {
-            /** @var DataSource $dataSource */
-            $dataSource = new $dataSourceClass();
-            SproutBase::$app->dataSources->saveDataSource($dataSource);
-        }
-
-        // Redirect to welcome page
         if (Craft::$app->getRequest()->getIsConsoleRequest()) {
             return;
         }
 
-        Craft::$app->controller->redirect(UrlHelper::cpUrl('sprout-forms/welcome'))->send();
+        // Redirect to welcome page
+        $url = UrlHelper::cpUrl('sprout/welcome/forms');
+        Craft::$app->controller->redirect($url)->send();
     }
 }
 
--- src/config.php
+++ src/config.php
@@ -6,100 +6,98 @@
  */
 
 /**
- * Sprout Forms config.php
+ * Forms settings available in craft/config/sprout.php
  *
- * This file exists only as a template for the Sprout Forms settings.
- * It does nothing on its own.
+ * This file does nothing on its own. It provides documentation of the
+ * default value for each config setting and provides an example of how to
+ * override each setting in 'craft/config/sprout.php`
  *
- * Don't edit this file, instead copy it to 'craft/config' as 'sprout-forms.php'
- * and make your changes there to override default settings.
+ * To override default settings, copy the settings you wish to implement to
+ * your 'craft/config/sprout.php' config file and make your changes there.
  *
- * Once copied to 'craft/config', this file will be multi-environment aware as
- * well, so you can have different settings groups for each environment, just as
- * you do for 'general.php'
- *
- * @noinspection ClassConstantCanBeUsedInspection
+ * Config settings files are multi-environment aware so you can have different
+ * settings groups for each environment, just as you do for 'general.php'
  */
-
 return [
-    // The name to display in the control panel in place of the plugin name
-    'pluginNameOverride' => 'Sprout Forms',
+    'sprout' => [
+        'forms' => [
+            // The default templates that will be used to output your forms when using
+            // the displayForm tag if no Form Templates are selected for a given form
+            'formTemplateId' => 'barrelstrength\sproutbase\app\forms\formtemplates\AccessibleTemplates',
 
-    // The default templates that will be used to output your forms when using
-    // the displayForm tag if no Form Templates are selected for a given form
-    'formTemplateId' => 'barrelstrength\sproutforms\formtemplates\AccessibleTemplates',
+            // Disable this setting to stop Sprout Forms from saving all
+            // form submission data to the Craft database
+            'enableSaveData' => true,
 
-    // Disable this setting to stop Sprout Forms from saving all
-    // form submission data to the Craft database
-    'enableSaveData' => true,
+            // Whether to save Entries flagged as spam to the database
+            'saveSpamToDatabase' => false,
 
-    // Whether to save Entries flagged as spam to the database
-    'saveSpamToDatabase' => false,
+            // The default behavior for saving data when a new Form is created
+            'enableSaveDataDefaultValue' => true,
 
-    // The default behavior for saving data when a new Form is created
-    'enableSaveDataDefaultValue' => true,
+            // Enable to capture the IP Address used when a Form Entry is saved
+            'trackRemoteIp' => false,
 
-    // Enable to capture the IP Address used when a Form Entry is saved
-    'trackRemoteIp' => false,
+            // The form section that will be selected by default in the
+            // sidebar navigation ('forms' or 'entries')
+            'defaultSection' => 'entries',
 
-    // The form section that will be selected by default in the
-    // sidebar navigation ('forms' or 'entries')
-    'defaultSection' => 'entries',
+            // Enable this setting to allow users to edit existing form entries in
+            // front-end templates. Enabling this feature may have some workflow or
+            // security considerations as forms allow anonymous submissions.
+            'enableEditFormEntryViaFrontEnd' => false,
 
-    // Enable this setting to allow users to edit existing form entries in
-    // front-end templates. Enabling this feature may have some workflow or
-    // security considerations as forms allow anonymous submissions.
-    'enableEditFormEntryViaFrontEnd' => false,
+            // The behavior your user will see if a submission is flagged as spam
+            // 'redirectAsNormal' - will simulate a successful submission and direct
+            //   the user to the Redirect Page.
+            // 'redirectBackToForm - will return the user to the form. All failed
+            //   captchas are logged on the Spam Entries saved in the database.
+            'spamRedirectBehavior' => 'redirectAsNormal',
 
-    // The behavior your user will see if a submission is flagged as spam
-    // 'redirectAsNormal' - will simulate a successful submission and direct
-    //   the user to the Redirect Page.
-    // 'redirectBackToForm - will return the user to the form. All failed
-    //   captchas are logged on the Spam Entries saved in the database.
-    'spamRedirectBehavior' => 'redirectAsNormal',
+            // The total number of Spam entries that will be stored in the database.
+            // When the limit is reached, the least recently updated Spam entry will
+            // be deleted from the database.
+            'spamLimit' => 500,
 
-    // The total number of Spam entries that will be stored in the database.
-    // When the limit is reached, the least recently updated Spam entry will
-    // be deleted from the database.
-    'spamLimit' => 500,
+            // The probability that the Spam cleanup task will run each time a
+            // Form Entry is saved. A lower probability will trigger a cleanup task
+            // less often and the number of Spam Entries stored in the database may
+            // be higher than the Spam Limit target until the cleanup task is triggered.
+            //
+            // 0 - None
+            // 100000 - 1 in 10
+            // 10000 - 1 in 100
+            // 1000 - selected>1 in 1,000
+            // 100 - 1 in 10,000
+            // 10 - 1 in 100,000
+            // 1 - 1 in 1,000,000
+            'cleanupProbability' => 1000,
 
-    // The probability that the Spam cleanup task will run each time a
-    // Form Entry is saved. A lower probability will trigger a cleanup task
-    // less often and the number of Spam Entries stored in the database may
-    // be higher than the Spam Limit target until the cleanup task is triggered.
-    //
-    // 0 - None
-    // 100000 - 1 in 10
-    // 10000 - 1 in 100
-    // 1000 - selected>1 in 1,000
-    // 100 - 1 in 10,000
-    // 10 - 1 in 100,000
-    // 1 - 1 in 1,000,000
-    'cleanupProbability' => 1000,
+            // Individual captcha settings
+            //
+            // Additional Captchas can be found in the Plugin Store:
+            // https://plugins.craftcms.com/sprout-forms-google-recaptcha
+            //
+            // Custom Captchas can be added via the Captcha API
+            'captchaSettings' => [
+                'barrelstrength\sproutforms\captchas\DuplicateCaptcha' => [
+                    'enabled' => true,
+                ],
+                'barrelstrength\sproutforms\captchas\JavascriptCaptcha' => [
+                    'enabled' => true,
+                ],
+                'barrelstrength\sproutforms\captchas\HoneypotCaptcha' => [
+                    'enabled' => true,
+                    'honeypotFieldName' => 'sprout-forms-hc',
+                    'honeypotScreenReaderMessage' => 'Leave this field blank'
+                ],
+            ],
 
-    // Individual captcha settings
-    //
-    // Additional Captchas can be found in the Plugin Store:
-    // https://plugins.craftcms.com/sprout-forms-google-recaptcha
-    //
-    // Custom Captchas can be added via the Captcha API
-    'captchaSettings' => [
-        'barrelstrength\sproutforms\captchas\DuplicateCaptcha' => [
-            'enabled' => true,
-        ],
-        'barrelstrength\sproutforms\captchas\JavascriptCaptcha' => [
-            'enabled' => true,
-        ],
-        'barrelstrength\sproutforms\captchas\HoneypotCaptcha' => [
-            'enabled' => true,
-            'honeypotFieldName' => 'sprout-forms-hc',
-            'honeypotScreenReaderMessage' => 'Leave this field blank'
+            // Show/Hide the Notifications tab in Sprout Forms
+            'showNotificationsTab' => true,
+
+            // Show/Hide the Reports tab in Sprout Forms
+            'showReportsTab' => true,
         ],
     ],
-
-    // Show/Hide the Notifications tab in Sprout Forms
-    'showNotificationsTab' => true,
-
-    // Show/Hide the Reports tab in Sprout Forms
-    'showReportsTab' => true,
 ];
